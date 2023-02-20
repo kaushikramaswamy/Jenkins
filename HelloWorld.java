ab62cc2dc2f76b3ce22b70a219fced0c254e52e9
Kaushik Ramaswamy <kaushik.ramaswamy@ymail.com>
2023-02-20 18:59:05 +1100

    removed

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Ola, Jello World!");
-        System.out.println("Ola, Zombie World!");
+        System.out.println("Ola, World!");
     }
 }
