ae5a51f901a4044236d72a07223064f21065ceed
Kaushik Ramaswamy <kaushik.ramaswamy@ymail.com>
2023-02-20 19:12:08 +1100

    New

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Ola, World!");
         System.out.println("Hello, World!");
+        System.out.println("Beautiful, World!");
     }
 }
