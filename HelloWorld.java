88cc843532949254ba3b060a0e893c940aac6e2a
Kaushik Ramaswamy <kaushik.ramaswamy@ymail.com>
2023-02-20 17:59:31 +1100

    HelloX2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Ola, Ola World!");
+        System.out.println("Ola, Hello World!");
         System.out.println("Ola, Hello World!");
     }
 }
